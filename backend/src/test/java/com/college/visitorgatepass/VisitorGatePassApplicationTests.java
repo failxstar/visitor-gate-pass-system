c6328395f2e25711faae5de579efa396f6e33f92
failxstar <sivag20060525@gmail.com>
2026-07-31 06:39:40 +0530

    chore: fix test package name and remove stale frontend folders

---
+++ backend/src/test/java/com/college/visitorgatepass/VisitorGatePassApplicationTests.java
@@ -0,0 +1,14 @@
+package com.college.visitorgatepass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("dev")
+class VisitorGatePassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
